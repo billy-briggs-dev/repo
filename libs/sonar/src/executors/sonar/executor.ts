2b27eb076ded5269382e382f237801c64d44a562
billy-briggs-dev <billybriggs33@gmail.com>
2022-07-02 22:37:26 -0500

    refactor options mapping method

--- libs/sonar/src/executors/sonar/executor.ts
+++ libs/sonar/src/executors/sonar/executor.ts
@@ -1,86 +1,29 @@
 import type { ExecutorContext } from '@nrwl/devkit';
 import { exec } from 'child_process';
 import { promisify } from 'util';
-
-export interface Project {
-  settings: string;
-}
-
-export interface SonarExecutorOptions {
-  project : {
-    settings: string;
-  },
-  sonar : {
-    host: {
-      url: string;
-    }
-    projectKey: string;
-    projectName: string;
-    projectVersion: string;
-    login: string;
-    password: string;
-    ws: {
-      timeout: string;
-    }
-    projectDescription: string;
-    links: {
-      homepage: string;
-      ci: string;
-      issue: string;
-      scm: string;
-    }
-    sources: string;
-    tests: string;
-    sourceEncoding: string;
-    externalIssuesReportPaths: string;
-    projectDate: string;
-    projectBaseDir: string;
-    working: {
-      directory: string;
-    }
-    scm: {
-      provider: string;
-      forceReloadAll: string;
-      exclusions: {
-        disabled: string;
-      }
-      revision: string;
-    }
-    buildString: string;
-    analysis: any
-    newCode: {
-      referenceBranch: string;
-    }
-    // sonar.cpd.${language}.minimumtokens	
-    // sonar.cpd.${language}.minimumLines
-    log: {
-      level: string;
-    }	
-    verbose: string;
-    scanner: {
-      dumpToFile: string;
-      metadataFilePath: string;
-    }
-    qualitygate: {
-      wait: string;
-      timeout: string;
-    }
-  }
-}
-
+import { SonarExecutorOptions } from './schema';
 
 export default async function sonarExecutor(
   options: SonarExecutorOptions,
-  context: ExecutorContext
-): Promise<{ success: boolean }> {
-  console.info(`Executing "sonar-scanner"...`);
-  console.info(`Options: ${JSON.stringify(options, null, 2)}`);
-  
+  _context: ExecutorContext
+): Promise<{ success: boolean }> {  
+  if (!options?.project?.settings) {
+    if (!options?.sonar?.projectKey) {
+      throw new Error('Project Key is required! Please provide a project key in the sonar properties file or in the project.json.');
+    }
+
+    if(!options?.sonar?.host?.url) {
+      throw new Error('Host Url is required! Please provide a host url in the sonar properties file or in the project.json.');
+    }
+  }
+
   const args: string[] = [];
-  getPaths(options).map((path) => {
-    args.push(`-D${path}=${getValue(options, path)}`);
+  getPathsAsStringArray(options).map((path) => {
+    args.push(`-D${path.join('.')}=${getValueByPath(options, path)}`);
   });
 
+  console.info(`Executing "sonar-scanner"...`);
+  console.info(`Options: ${JSON.stringify(options, null, 2)}`);
   const { stdout, stderr } = await promisify(exec)(
     `sonar-scanner ${args.join(' ')}`
   );
@@ -92,22 +35,22 @@ export default async function sonarExecutor(
   return { success };
 }
 
-// get paths to all nested keys in an object where the value is a string
-function getPaths(obj: any, path: string[] = []): string[] {
-  const paths: string[] = [];
+// Get all paths as a string array in a nested object
+function getPathsAsStringArray(obj: any): string[][] {
+  const paths: string[][] = [];
   Object.keys(obj).map((key) => {
     if (typeof obj[key] === 'string') {
-      paths.push(path.concat(key).join('.'));
+      paths.push([key]);
     } else if (typeof obj[key] === 'object') {
-      getPaths(obj[key], path.concat(key)).map((p) => {
-        paths.push(p);
+      getPathsAsStringArray(obj[key]).map((p) => {
+        paths.push([key, ...p]);
       })
     }
   })
   return paths;
 }
 
-// Get value of a key in an object
-function getValue(obj: any, path: string): any {
-  return obj[path];
+// Get nested value from object
+function getValueByPath(obj: any, path: string[]): string {
+  return path.reduce((o, k) => o[k], obj);
 }
--- libs/sonar/src/executors/sonar/schema.d.ts
+++ libs/sonar/src/executors/sonar/schema.d.ts
@@ -1,1 +1,86 @@
-export interface SonarExecutorSchema {} // eslint-disable-line
+export interface Project {
+    settings: string;
+}
+  
+export interface Project {
+    settings: string;
+}
+
+export interface Host {
+    url: string;
+}
+
+export interface WS {
+    timeout: string;
+}
+
+export interface Links {
+    homepage: string;
+    ci: string;
+    scm: string;
+}
+
+export interface Working {
+    directory: string;
+}
+
+export interface SCM {
+    provider: string;
+    forceReloadAll: string;
+    exclusions: {
+        disabled: string;
+    }
+    revision: string;
+}
+
+export interface NewCode {
+    referenceBranch: string;
+}
+
+export interface Log {
+    level: string;
+}	
+
+export interface Scanner  {
+    dumpToFile: string;
+    metadataFilePath: string;
+}
+
+export interface QualityGate {
+    wait: string;
+    timeout: string;
+}
+
+export interface Sonar {
+    host: Host;
+    projectKey: string;
+    projectName: string;
+    projectVersion: string;
+    login: string;
+    password: string;
+    ws: WS;
+    projectDescription: string;
+    links: Links;
+    sources: string;
+    tests: string;
+    sourceEncoding: string;
+    externalIssuesReportPaths: string;
+    projectDate: string;
+    projectBaseDir: string;
+    working: Working;
+    scm: SCM;
+    buildString: string;
+    analysis: any
+    newCode: NewCode;
+    // sonar.cpd.${language}.minimumtokens	
+    // sonar.cpd.${language}.minimumLines
+    log: Log;
+    verbose: string;
+    scanner: Scanner;
+    qualitygate: QualityGate;
+}
+
+export interface SonarExecutorOptions {
+    project: Project;
+    sonar: Sonar;
+}
